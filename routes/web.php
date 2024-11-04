6b8990e3c1d44b2037a6105dc327e4d285fc5065
pipisa <your@email.ex>
2024-11-04 14:35:38 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/blog', [PostsController::class, 'index']);
+Route::get('/blog/create', [PostsController::class, 'create'])->name('blog.create');
+Route::get('/blog/{slug}', [PostsController::class, 'show'])->name('blog.show');
+Route::post('/blog/store', [PostsController::class, 'store'])->name('blog.store');
+Route::get('/blog/{slug}/edit', [PostsController::class, 'edit'])->name('blog.edit');
+Route::PUT('/blog/{slug}/update', [PostsController::class, 'update'])->name('blog.update');
+Route::delete('/blog/{slug}/delete', [PostsController::class, 'delete'])->name('blog.delete');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
